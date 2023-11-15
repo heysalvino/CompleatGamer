725d44fb4431a63598913bb818cbeb69fc870a63
Jessica Salvino <150205230+heysalvino@users.noreply.github.com>
2023-11-14 21:31:08 -0500

    Update passe_dix.py

--- passe_dix.py
+++ passe_dix.py
@@ -1,36 +1,31 @@
-# Estimate the value of winning Passe-Dix the Monte Carlo simulation
-
 from random import randint
 
-def main():
+def estimate_winning_passe_dix():
+    """
+    Estimate the chance of winning Passe-Dix using Monte Carlo simulation.
+    Outputs: Estimated winning percentage
+    """
+    # Parameters
+    num_iterations = 1000000
 
-  '''
-     Main method to estimate winning Passe-Dix
-     inputs:none
-     output: print the estimate
-  '''
-  
-  # Parameters
-  num_iterations = 1000000
-  
-  # State variables
-  num_wins = 0
-  
-  # Loop over all simulated trials
-  for iteration in range(num_iterations):
-      dice_one = randint(1,6)
-      dice_two = randint(1,6)
-      dice_three = randint(1,6)
-      
-      result = dice_one + dice_two + dice_three
-      
-      if result > 10:
-        num_wins += 1
-        
-  # Post trial calculations
-  estimate = float(num_wins) / float(num_iterations) * 100
-  print " Your chance of winning Passe-Dix is %.2f percent." % (estimate)
-  
-  # Run main if the script is run from the terminal 
+    # State variables
+    num_wins = 0
+
+    # Loop over all simulated trials
+    for iteration in range(num_iterations):
+        dice_one = randint(1, 6)
+        dice_two = randint(1, 6)
+        dice_three = randint(1, 6)
+
+        result = dice_one + dice_two + dice_three
+
+        if result > 10:
+            num_wins += 1
+
+    # Post-trial calculations
+    estimate = (num_wins / num_iterations) * 100
+    print(f'Your chance of winning Passe-Dix is {estimate:.2f} percent.')
+
+# Run the estimation if the script is run from the terminal
 if __name__ == '__main__':
-    main()
+    estimate_winning_passe_dix()
